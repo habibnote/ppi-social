25c98be973430ca866097d4b15f5873d89d2aff1
habibnote <habibblog97@gmail.com>
2023-12-10 12:01:38 +0600

    initr

---
+++ template/home-template.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Home Page
+ */
+?>
+<header class="header-warpper">
+    <div class="habib-container">
+        <div class="logo-area">
+            <a href="#">
+                Hello
+            </a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+                <li><a href="#">home</a></li>
+            </ul>
+        </div>
+    </div>
+</header>
